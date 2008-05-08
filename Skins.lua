6a808eb8bb946e3a1c682ee62b3843e809ce97f7
StormFX <stormfx@gmail.com>
2008-05-08 07:18:12 +0000

    - Update files.

---
+++ Skins.lua
@@ -0,0 +1,99 @@
+--[[ LiteStep for ButtonFacade ]]
+
+local LibButtonFacade = LibStub("LibButtonFacade",true)
+if not LibButtonFacade then
+	return
+end
+
+-- LiteStep
+LibButtonFacade:AddSkin("LiteStep",{
+
+	-- Skin data start.
+	Backdrop = {
+		Width = 36,
+		Height = 36,
+		Texture = [[Interface\AddOns\ButtonFacade_LiteStep\Textures\Backdrop]],
+	},
+	Icon = {
+		Width = 32,
+		Height = 32,
+		TexCoords = {0.07,0.93,0.07,0.93},
+	},
+	Border = {
+		Width = 36,
+		Height = 36,
+		Texture = [[Interface\AddOns\ButtonFacade_LiteStep\Textures\Equip]],
+		BlendMode = "ADD",
+	},
+	Flash = {
+		Width = 36,
+		Height = 36,
+		Texture = [[Interface\AddOns\ButtonFacade_LiteStep\Textures\Flash]],
+	},
+	Cooldown = {
+		Width = 32,
+		Height = 32,
+	},
+	AutoCast = {
+		Width = 32,
+		Height = 32,
+		ModelScale = 1.0,
+	},
+	AutoCastable = {
+		Width = 64,
+		Height = 64,
+		Texture = [[Interface\Buttons\UI-AutoCastableOverlay]],
+		OffsetX = 0.5,
+		OffsetY = -0.5,
+	},
+	Normal = {
+		Width = 36,
+		Height = 36,
+		Texture = [[Interface\AddOns\ButtonFacade_LiteStep\Textures\Normal]],
+		Static = true,
+	},
+	Pushed = {
+		Width = 36,
+		Height = 36,
+		Texture = [[Interface\AddOns\ButtonFacade_LiteStep\Textures\Pushed]],
+	},
+	Disabled = {
+		Hide = true,
+	},
+	Checked = {
+		Width = 36,
+		Height = 36,
+		Texture = [[Interface\AddOns\ButtonFacade_LiteStep\Textures\Checked]],
+		BlendMode = "ADD",
+	},
+	Gloss = {
+		Width = 36,
+		Height = 36,
+		Texture = [[Interface\AddOns\ButtonFacade_LiteStep\Textures\Gloss]],
+	},
+	Highlight = {
+		Width = 36,
+		Height = 36,
+		Texture = [[Interface\AddOns\ButtonFacade_LiteStep\Textures\Highlight]],
+		BlendMode = "ADD",
+	},
+	HotKey = {
+		Width = 36,
+		Height = 10,
+		OffsetX = -2,
+		OffsetY = 10,
+	},
+	Count = {
+		Width = 36,
+		Height = 10,
+		OffsetX = -2,
+		OffsetY = -10,
+	},
+	Name = {
+		Width = 36,
+		Height = 10,
+		OffsetY = -10,
+	},
+	-- Skin data end.
+
+},true)
