4af823ab4847bcafdf17f76a8bea1120f52a6f20
StormFX <stormfx@gmail.com>
2010-03-14 15:53:59 +0000

    - Tweaks

--- Skins.lua
+++ Skins.lua
@@ -1,14 +1,10 @@
 --[[ LiteStep @project-version@ ]]
 
 local LBF = LibStub("LibButtonFacade", true)
-if not LBF then
-	return
-end
+if not LBF then return end
 
 -- LiteStep
 LBF:AddSkin("LiteStep", {
-
-	-- Skin data start.
 	Backdrop = {
 		Width = 36,
 		Height = 36,
@@ -20,22 +16,21 @@ LBF:AddSkin("LiteStep", {
 		Height = 32,
 		TexCoords = {0.07,0.93,0.07,0.93},
 	},
+	Pushed = {
+		Width = 36,
+		Height = 36,
+		Color = {0, 0, 0, 0.5},
+		Texture = [[Interface\AddOns\ButtonFacade_LiteStep\Textures\Overlay]],
+	},
 	Flash = {
 		Width = 36,
 		Height = 36,
-		Color = {1, 0, 0, 0.5},
+		Color = {1, 0, 0, 0.3},
 		Texture = [[Interface\AddOns\ButtonFacade_LiteStep\Textures\Overlay]],
 	},
 	Cooldown = {
 		Width = 32,
 		Height = 32,
-	},
-	AutoCast = {
-		Width = 32,
-		Height = 32,
-		OffsetX = 1,
-		OffsetY = -1,
-		AboveNormal = true,
 	},
 	Normal = {
 		Width = 36,
@@ -44,11 +39,15 @@ LBF:AddSkin("LiteStep", {
 		Color = {1, 1, 1, 1},
 		Texture = [[Interface\AddOns\ButtonFacade_LiteStep\Textures\Normal]],
 	},
-	Pushed = {
+	Disabled = {
+		Hide = true,
+	},
+	Checked = {
 		Width = 36,
 		Height = 36,
-		Color = {0, 0, 0, 0.5},
-		Texture = [[Interface\AddOns\ButtonFacade_LiteStep\Textures\Overlay]],
+		BlendMode = "ADD",
+		Color = {0, 0.8, 1, 0.8},
+		Texture = [[Interface\AddOns\ButtonFacade_LiteStep\Textures\Border]],
 	},
 	Border = {
 		Width = 36,
@@ -57,15 +56,19 @@ LBF:AddSkin("LiteStep", {
 		Color = {0, 1, 0, 0.3},
 		Texture = [[Interface\AddOns\ButtonFacade_LiteStep\Textures\Border]],
 	},
-	Disabled = {
-		Hide = true,
-	},
-	Checked = {
+	Highlight = {
 		Width = 36,
 		Height = 36,
 		BlendMode = "ADD",
-		Color = {0, 0.75, 1, 1},
+		Color = {1, 1, 1, 0.5},
 		Texture = [[Interface\AddOns\ButtonFacade_LiteStep\Textures\Border]],
+	},
+	AutoCast = {
+		Width = 32,
+		Height = 32,
+		OffsetX = 1,
+		OffsetY = -1,
+		AboveNormal = true,
 	},
 	AutoCastable = {
 		Width = 64,
@@ -73,13 +76,6 @@ LBF:AddSkin("LiteStep", {
 		OffsetX = 0.5,
 		OffsetY = -0.5,
 		Texture = [[Interface\Buttons\UI-AutoCastableOverlay]],
-	},
-	Highlight = {
-		Width = 36,
-		Height = 36,
-		BlendMode = "ADD",
-		Color = {1, 1, 1, 1},
-		Texture = [[Interface\AddOns\ButtonFacade_LiteStep\Textures\Border]],
 	},
 	Gloss = {
 		Width = 36,
@@ -103,14 +99,10 @@ LBF:AddSkin("LiteStep", {
 		Height = 10,
 		OffsetY = -10,
 	},
-	-- Skin data end.
-
 }, true)
 
 -- LiteStep XLT
 LBF:AddSkin("LiteStep XLT", {
-
-	-- Skin data start.
 	Template = "LiteStep",
 	Normal = {
 		Width = 36,
@@ -119,6 +111,4 @@ LBF:AddSkin("LiteStep XLT", {
 		Color = {1, 1, 1, 1},
 		Texture = [[Interface\AddOns\ButtonFacade_LiteStep\Textures\Lite]],
 	},
-	-- Skin data end.
-
 }, true)
